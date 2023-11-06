fc2e9f56aa41825a17cc05035b6b4b3c3028b3c5
K3nnneth <K.bui2@yahoo.com>
2023-11-06 15:53:25 -0800

    Added google maps for iOS and Android

--- GeoNewsFinder4/App.js
+++ GeoNewsFinder4/App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { WebView } from 'react-native-webview';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
-  return (
+  return Platform.OS === "web" ? (
+    <iframe
+    width="1000"
+    height="1000"
+    style="border:0"
+    loading="lazy"
+    allowfullscreen
+    referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBzqOAEFXewORNVWDH8UKRueMF9boPygbs
+      &q=Space+Needle,Seattle+WA">
+  </iframe>
+  ) : (
     <View style={styles.container}>
-      <Text>This is our environment for geonewsfinder69! :D</Text>
-      <StatusBar style="auto" />
+    <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +29,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
