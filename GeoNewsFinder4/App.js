735730a619de6785e939b50fdc59df5ddca77e26
K3nnneth <K.bui2@yahoo.com>
2023-11-13 10:10:57 -0800

    Removed unfinished/unused code

--- GeoNewsFinder4/App.js
+++ GeoNewsFinder4/App.js
@@ -1,24 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
-import { WebView } from 'react-native-webview';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
-  return Platform.OS === "web" ? (
-    <iframe
-    width="1000"
-    height="1000"
-    style="border:0"
-    loading="lazy"
-    allowfullscreen
-    referrerpolicy="no-referrer-when-downgrade"
-    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBzqOAEFXewORNVWDH8UKRueMF9boPygbs
-      &q=Space+Needle,Seattle+WA">
-  </iframe>
-  ) : (
+  return (
     <View style={styles.container}>
-    <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
     </View>
   )
 }
