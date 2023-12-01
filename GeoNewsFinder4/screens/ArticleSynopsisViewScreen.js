9a76a92425e3293a0d08cd8a1f5efca1e71da289
K3nnneth <K.bui2@yahoo.com>
2023-11-30 23:36:30 -0800

    Added 3 tab view template

--- GeoNewsFinder4/screens/ArticleSynopsisViewScreen.js
+++ GeoNewsFinder4/screens/ArticleSynopsisViewScreen.js
@@ -1,9 +1,55 @@
 import React from 'react';
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet, useWindowDimensions} from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 function ArticleSynopsisView() {
+  const OverviewRoute = () => (
+    <View style={styles.overviewContainer} />
+  );
+  
+  const ChatBotRoute = () => (
+    <View style={styles.chatBotContainer} />
+  );
+
+  const RelatedTilesRoute = () => (
+    <View style={styles.relatedTilesContainer} />
+  );
+  
+  const renderScene = SceneMap({
+    first: OverviewRoute,
+    second: ChatBotRoute,
+    third: RelatedTilesRoute,
+  });
+
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'Overview' },
+    { key: 'second', title: 'Chat Bot' },
+    { key: 'third', title: 'Related' },
+  ]);
+
+  const renderTabBar = (props) => (
+    <TabBar
+      {...props}
+      style={styles.tabBar}
+      indicatorStyle={styles.tabBarIndicator}
+      labelStyle={styles.tabBarLabel}
+    />
+  );
+
   return (
     <View style={styles.container}>
+      <View style={styles.tabViewContainer}>
+        <TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: layout.width }}
+          renderTabBar={renderTabBar}
+        />
+      </View>
     </View>
   );
 }
@@ -14,6 +60,36 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    tabViewContainer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: '65%',
+    },
+    tabBar: {
+      backgroundColor: 'teal',
+      height: '9%',
+    },
+    tabBarIndicator: {
+      backgroundColor: 'white',
+    },
+    tabBarLabel: {
+      fontSize: 12,
+    },
+    overviewContainer: {
+      flex: 1,
+      backgroundColor: 'grey',
+    },
+    chatBotContainer: {
+      flex: 1,
+      backgroundColor: 'red',
+    },
+    relatedTilesContainer: {
+      flex: 1,
+      backgroundColor: 'blue',
+    },
   });
 
 export default ArticleSynopsisView;
+
