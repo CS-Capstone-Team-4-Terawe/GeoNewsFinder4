864f11a76bede7ad2b9deb694392f46da7a4a554
K3nnneth <K.bui2@yahoo.com>
2023-12-01 10:42:05 -0800

    Added clear button to search bar

--- GeoNewsFinder4/components/SearchBar.js
+++ GeoNewsFinder4/components/SearchBar.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text, View} from 'react-native';
 
 const SearchBar = ({ onSearchSubmit }) => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const clearSearchInput = () => {
+    setSearchInput('');
+  };
+
+  const handlSearchInput = (text) => {
+    setSearchInput(text)
+  };
+
     return (
       <SafeAreaView>
-        <TextInput
-          style={styles.input}
-          placeholder="Search location/topic..."
-          onSubmitEditing={onSearchSubmit}
-        />
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Search location/topic..."
+            onSubmitEditing={onSearchSubmit}
+            onChangeText={handlSearchInput}
+            value={searchInput}
+          />
+          <TouchableOpacity style={styles.clearButton} onPress={clearSearchInput}>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     );
   };
@@ -18,11 +35,29 @@ const SearchBar = ({ onSearchSubmit }) => {
     input: {
       height: 40,
       width: 300,
-      margin: 12,
-      borderWidth: 1,
+      margin: 0,
       padding: 10,
+      borderWidth: 1,
+      borderRightWidth: 0,
       borderRadius: 15,
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
       backgroundColor: 'white',
+    },
+    searchContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    clearButton: {
+      margin: 0,
+      height: 40,
+      padding: 10,
+      backgroundColor: 'white',
+      borderWidth: 1,
+      borderLeftWidth: 0,
+      borderRadius: 15,
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0,
     },
   });
   
